b188f4cb93c10337209fc719f55b27bb9d321586
Mikjail Salazar <mikjailsalazar@gmail.com>
2018-09-08 21:44:38 +0400

    Swan-JS-Card #comment adding real content rendering

--- src/pages/home/view1/controller.ts
+++ src/pages/home/view1/controller.ts
@@ -1,7 +1,25 @@
 export default class ListRepoController {
     listRepo = []
+    scope
 
     constructor($scope) {
-        this.listRepo = $scope.$parent.$ctrl.listRepo
+     this.scope = $scope
+     this.initList()
+    }
+
+    initList() {
+        this.listRepo =   this.scope.$parent.$ctrl.listRepo.map((
+            {name, description, url, language, clone_url, forks_count, stargazers_count, open_issues_count }) => {
+            return {
+                name,
+                language,
+                description,
+                url,
+                clone_url,
+                forks_count,
+                stargazers_count,
+                open_issues_count }
+        })
+        console.log(this.listRepo)
     }
 }
